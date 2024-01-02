0baae7ca6d9757840a9d72904d663aa522adf86a
Ronaldo Caldas <ronaldocaldas@gmail.com>
2024-01-02 14:54:01 -0300

    wip: inicial project commit

---
+++ src/main/java/com/mobi7/positionapi/PositionApiApplication.java
@@ -0,0 +1,13 @@
+package com.mobi7.positionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositionApiApplication.class, args);
+	}
+
+}
